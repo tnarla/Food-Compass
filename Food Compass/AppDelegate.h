91c069711a3134db12da7259b37241ab1b65e96b
tnarla <trushithanarla@gmail.com>
2015-09-11 19:56:33 -0400

    Initial Commit

---
+++ Food Compass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Food Compass
+//
+//  Created by Trushitha Narla on 9/11/15.
+//  Copyright (c) 2015 Trushitha Narla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Food Compass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Food Compass
+//
+//  Created by Trushitha Narla on 9/11/15.
+//  Copyright (c) 2015 Trushitha Narla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
